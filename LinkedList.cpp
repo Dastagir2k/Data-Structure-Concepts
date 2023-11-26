b4025e393ee37f19a8ddc2885041616738f96dff
Dastagir2k <dastagir2k@gmail.com>
2023-11-26 18:55:25 +0530

    Converting array into linkedlist

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,29 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
+struct Node
+{
     int data;
-    Node* next;
-    Node(int data1){
-        data=data1;
-        next=nullptr;
+    Node *next;
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
     }
 };
 
-int main(){
+Node *convertArr(vector<int> &arr)
+{
+    // Create a new node and assign it a value
+    Node *Head = new Node(arr[0]);
+
+    // Keep track of the last node visited
+    Node *mover = Head;
+
+    // Iterate through the array starting from the second element
+    for (int i = 1; i < arr.size(); i++)
+    {
+        // Create a new node and assign it a value
+        Node *temp = new Node(arr[i]);
+
+        // Connect the new node to the previous node
+        mover->next = temp;
+
+        // Move the 'mover' to the new node
+        mover = temp;
+    }
+
+    // Return the head of the new linked list
+    return Head;
+}
+int main()
+{
     // initializing array with given values
-    vector<int>arr={2,4,6,8};
+    vector<int> arr = {2, 4, 6, 8};
 
-    // creating new Node with the first element of array
-    Node* Head= new Node(arr[0]);
-    
- 
-
-    // printing the Head's data
-    cout<<"Head -->"<<Head->data<<endl;
-
-    // printing the Head's next pointer (nullptr in this case)
-    cout<<"Head Next-->"<<Head->next;
+    Node *Head = convertArr(arr);
+    cout << Head->data;
 
     return 0;
 }
